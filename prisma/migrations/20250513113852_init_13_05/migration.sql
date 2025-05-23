068716e377594ce01ad367fd17f87f2e32afe71f
koladiyaraj22 <koladiyaraj22@gmail.com>
2025-05-23 10:47:12 +0530

    mac

--- prisma/migrations/20250513113852_init_13_05/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "renewPeriod" AS ENUM ('monthly', 'quarterly', 'yearly', 'half_yearly', 'custom');
-
--- AlterTable
-ALTER TABLE "customer_product_history" ADD COLUMN     "renewPeriod" "renewPeriod" NOT NULL DEFAULT 'custom';
--- src/generated/client/index.js
+++ src/generated/client/index.js
@@ -431,7 +431,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\CPM\\CPM-Backend-Prisma\\src\\generated\\client",
+      "value": "/Users/rajkoladiya/Projects/CPM/Dashbord-API/src/generated/client",
       "fromEnvVar": null
     },
     "config": {
@@ -440,7 +440,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin-arm64",
         "native": true
       },
       {
@@ -449,11 +449,11 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\CPM\\CPM-Backend-Prisma\\prisma\\schema.prisma",
+    "sourceFilePath": "/Users/rajkoladiya/Projects/CPM/Dashbord-API/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -467,7 +467,7 @@ const config = {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:Raj24315@localhost:5636/cpm_orm"
+        "value": "postgresql://postgres:24315@localhost:5636/CPM"
       }
     }
   },
@@ -509,6 +509,10 @@ warnEnvConflicts({
 const PrismaClient = getPrismaClient(config)
 exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
+
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
+path.join(process.cwd(), "src/generated/client/libquery_engine-darwin-arm64.dylib.node")
 
 // file annotations for bundling tools to include these files
 path.join(__dirname, "query_engine-windows.dll.node");
